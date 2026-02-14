e71d95fd07a451d531ab11493b8334d45d5b403e
ThinhPhoenix <laithinh1310@gmail.com>
2026-02-14 07:56:07 +0700

    support old devices

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['ios >= 12'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome61',
+  },
 })
